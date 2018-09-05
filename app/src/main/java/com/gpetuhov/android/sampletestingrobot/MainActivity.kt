62b74b9f5a0ca7835c4d5998ed62af0f83fd7ae7
Gennady Petukhov <gpetukhov@phoenixit.ru>
2018-09-05 12:50:16 +0300

    Add business logic

--- app/src/main/java/com/gpetuhov/android/sampletestingrobot/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampletestingrobot/MainActivity.kt
@@ -2,11 +2,18 @@ package com.gpetuhov.android.sampletestingrobot
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        processButton.setOnClickListener {
+            resultText.text = String.format("Hello, %s!", nameInput.text)
+            resultText.visibility = View.VISIBLE
+        }
     }
 }
